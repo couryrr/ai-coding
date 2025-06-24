9d7689d6fbf72d6dfb85704f7b26bc46cd6bbab1
Coury Richards <2702992+couryrr@users.noreply.github.com>
2025-06-24 14:52:45 -0400

    dev update: this was bothering me.

--- src/main/java/github/couryrr/aicoding/AiCodingApplication.java
+++ src/main/java/github/couryrr/aicoding/AiCodingApplication.java
@@ -3,6 +3,10 @@ package github.couryrr.aicoding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Customer Management API", version = "1.0.0", description = "RESTful API for managing customers and their purchases"))
 @SpringBootApplication
 public class AiCodingApplication {
     public static void main(String[] args) {
